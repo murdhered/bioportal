a34a306da2ce2d2cbcbfec7e63237e2412ad068f
murdhered <micaellobato530@gmail.com>
2025-06-11 16:03:46 +0100

    Initial commit

---
+++ src/components/buttons/LoginWithGoogle.js
@@ -0,0 +1,22 @@
+'use client';
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signIn } from "next-auth/react";
+
+export default function LoginWithGoogle() {
+  const handleLogin = () => {
+    signIn('google', {
+      callbackUrl: `${window.location.origin}/account`, // Explicit redirect to /account
+    });
+  };
+
+  return (
+    <button
+      onClick={handleLogin}
+      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center"
+    >
+      <FontAwesomeIcon icon={faGoogle} className="h-6" />
+      <span>Sign In with Google</span>
+    </button>
+  );
+}
