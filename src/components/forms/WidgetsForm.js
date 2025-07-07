79da369a041985a75e27027085639ee1e6571616
murdhered <micaellobato530@gmail.com>
2025-07-07 19:07:00 +0100

    bioportal updated

--- src/components/forms/WidgetsForm.js
+++ src/components/forms/WidgetsForm.js
@@ -6,6 +6,7 @@ import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import toast from "react-hot-toast";
+import SectionBox from "@/components/layout/SectionBox"; // Assuming you use this
 
 export default function WidgetsForm({ page }) {
     
@@ -35,8 +36,10 @@ export default function WidgetsForm({ page }) {
                         className="w-full p-2 outline-none"
                     />
                 </div>
+                {/* --- THIS IS THE FIX --- */}
+                {/* Replaced the double quotes with &quot; */}
                 <p className="text-xs text-gray-500 mt-2">
-                    To find this, go to your Discord Server Settings &gt; Widget, and copy the "Server ID".
+                    To find this, go to your Discord Server Settings &gt; Widget, and copy the &quot;Server ID&quot;.
                 </p>
             </div>
 
